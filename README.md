a15207810d22274c704c6ec382c4081bcfddd084
Handle Home <handle.huang@outlook.com>
2023-06-04 13:11:36 +0800

    add sect access key

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ Rename key.json.default to key.json and set the correct value where you get the 
 
 then have fun.
 
+## Before start the server
+ - get your key.json form azure
+ - rename .env.default to .env and change the sectAccessKey value
+
 ## How to start
 
 in your console run "npm run start".
--- app.js
+++ app.js
@@ -11,13 +11,39 @@ dotenv.config()
 // Enable cross-origin resource sharing
 app.use(cors())
 app.use(express.json())
+
+const checkAuthorization = (req, res, next) => {
+  // 检查 Authorization 值
+  const authorization = req.headers.authorization;
+
+  if (!authorization) {
+    return res.status(401).json({ error: 'Authorization header missing' });
+  }
+
+  // 在这里执行任何其他身份验证逻辑，例如验证令牌的有效性
+  const token = req.headers.authorization.split(' ')[1];
+
+  // 检查令牌是否与环境变量中的 sectAccessKey 相匹配
+  if (token !== process.env.sectAccessKey) {
+    return res.status(403).json({ error: 'Invalid authorization token' });
+  }
+
+  // 继续处理下一个中间件或路由处理程序
+  next();
+};
+
 // Serve static files from the public directory
 app.use(express.static('public', { index: 'index.html' }))
+// 应用中间件到指定路由
+app.use('/v1/chat/completions', checkAuthorization);
 
 // create a post route named /v1/chat/completions
 app.post('/v1/chat/completions', async (req, res) => {
   // return a JSON response with the following structure
   // console.log(req.body)
+  // here add a auth check
+  
+
   const { messages } = req.body
   const ret = await queryGpt4(messages)
   res.send(ret)
